f8a0f36e0796d2340c5c0f1a42f56597d0e55148
Nick Gibb <gibbnick@gmail.com>
2018-05-16 08:15:39 -0400

    added open layers instead of G Maps

--- src/components/map.js
+++ src/components/map.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
-import GoogleMapReact from 'google-map-react';
-import { Container } from 'semantic-ui-react'
-import Popup from './popup.js'
+import { Container } from "semantic-ui-react";
+import Popup from "./popup.js";
+import {
+  interaction,
+  layer,
+  custom,
+  control, //name spaces
+  Interactions,
+  Overlays,
+  Controls, //group
+  Map,
+  Layers,
+  Overlay,
+  Util //objects
+} from "react-openlayers";
 
 // const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export default class Map extends Component {
+export default class TheMap extends Component {
   static defaultProps = {
     center: {
       lat: 46.8139,
-      lng: -71.2080
+      lng: -71.208
     },
     zoom: 11
   };
@@ -23,38 +35,13 @@ export default class Map extends Component {
           </p>
         </h3>
 
-
         <Container fluid>
-          <div style={{ height: '75vh', width: '100%' }}>
-            <GoogleMapReact
-              defaultCenter={this.props.center}
-              defaultZoom={this.props.zoom}
-            >
-              <Popup
-                lat={46.8139}
-                lng={-71.5080}
-                content={'Can enter stuff here'}
-              />
-                            <Popup
-                lat={46.5139}
-                lng={-71.5080}
-                content={'This is a test'}
-              />
-                            <Popup
-                lat={46.7139}
-                lng={-71.3080}
-                content={'Here is some info'}
-              />
-                            <Popup
-                lat={46.8139}
-                lng={-71.2080}
-                content={'Can enter stuff here'}
-              />
-            </GoogleMapReact>
-          </div>
+          <Map view={{ center: [0, 0], zoom: 2 }}>
+            <Layers>
+              <layer.Tile />
+            </Layers>
+          </Map>
         </Container>
-
-
       </div>
     );
   }
